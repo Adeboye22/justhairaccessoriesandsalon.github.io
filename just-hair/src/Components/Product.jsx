c2424ffd4732e8a6ca3da98a0aa07517123f9b68
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-05-24 23:17:45 +0100

    just-hair

--- just-hair/src/Components/Product.jsx
+++ just-hair/src/Components/Product.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../styles/product.css'
+import Bigen from '../assets/Bigen.jpg'
 
 function Product() {
     return (
       <div>
-        <section className="wigs">
-
+        <section className="hairCare">
+          <div className="hairCare_1">
+            <img src={Bigen} alt="" />
+            <article>
+              <span></span>
+            </article>
+            <button type="submit">Buy now</button>
+          </div>
+          <div>
+            <article></article>
+            <span></span>
+            <button type="submit"></button>
+          </div>
+          <div>
+            <article></article>
+            <span></span>
+            <button type="submit"></button>
+          </div>
+          <div>
+            <article></article>
+            <span></span>
+            <button type="submit"></button>
+          </div>
         </section>
       </div>
     )
