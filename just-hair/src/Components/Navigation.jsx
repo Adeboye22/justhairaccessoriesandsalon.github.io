f81c657c153e796b8d7fc6f167dfa0cc29b2fe86
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-05-29 03:43:57 +0100

    just-hair

--- just-hair/src/Components/Navigation.jsx
+++ just-hair/src/Components/Navigation.jsx
@@ -3,7 +3,6 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import JustHairLogo from '../assets/JustHairLogo.png';
 import '../styles/navigation.css';
 import {Link, NavLink, useNavigate} from 'react-router-dom';
-// import WhatsApp from "https://api.whatsapp.com/send/?phone=2347086582442&text&type=phone_number&app_absent=0"
 
 //this component the header file that links to all the pages of the app 
 function Navigation() {
@@ -18,9 +17,6 @@ function Navigation() {
     navigate('/');
   }
 
-  const whatsAppRedirect = () =>{
-    navigate()
-  }
   return (
     <div>
       <header> 
@@ -34,7 +30,7 @@ function Navigation() {
             <li><NavLink to="Product" className="products">Products</NavLink></li>
             <li><NavLink to="Review" className="reviews" >Reviews</NavLink></li>
           </ul>
-          <button className= "contactUs" onClick = {whatsAppRedirect} target='_blank'>Contact Us</button>
+          <button className= "contactUs"> <a href="https://api.whatsapp.com/send/?phone=2348161512011&text&type=phone_number&app_absent=0" target='_blank'>Contact Us</a></button>
           <button className="navLogInBtn" onClick={routeChange}>Login</button>
           <button className="navCloseBtn" onClick={showNavBar}><FaTimes /></button>
         </nav>
